c38a6b951b07ce463ece5e6b2bdf529b6c088629
ByMyKolaps <muradymov.bulat@mail.ru>
2023-09-22 22:52:27 +0300

    CQRS-AutoMapper-Added

---
+++ Notes.Application/Exceptions/NotFoundException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Notes.Application.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string name, object key)
+            : base($"Entity \"{name}\" ({key}) not found") { }
+    }
+}
---
+++ Notes.Application/Notes/Queries/GetNoteDetails/NoteDetailsVm.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Notes.Application.Mappings;
+using Notes.Domain;
+using System;
+
+namespace Notes.Application.Notes.Queries.GetNoteDetails
+{
+    public class NoteDetailsVm : IMapWith<Note>
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Details { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime? EditDate { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<Note, NoteDetailsVm>()
+                .ForMember(noteVm => noteVm.Title,
+                    options => options.MapFrom(note => note.Title))
+                .ForMember(noteVm => noteVm.Details,
+                    options => options.MapFrom(note => note.Details))
+                .ForMember(noteVm => noteVm.Id,
+                    options => options.MapFrom(note => note.Id))
+                .ForMember(noteVm => noteVm.CreationDate,
+                    options => options.MapFrom(note => note.CreationDate))
+                .ForMember(noteVm => noteVm.EditDate,
+                    options => options.MapFrom(note => note.EditDate));
+        }
+    }
+}
